7915e31494e43620a8c56516cff942e715447566
ElTinmar <martin.privat@hotmail.fr>
2025-04-11 17:03:07 +0200

    custom error

--- ds18b20/__init__.py
+++ ds18b20/__init__.py
@@ -1,5 +1,8 @@
 import serial
 
+class CommunicationError(serial.SerialException):
+    ... 
+
 def read_temperature_celsius() -> float:
     ser = serial.Serial('/dev/ttyUSB0', baudrate = 9600)
     return float(ser.readline())
