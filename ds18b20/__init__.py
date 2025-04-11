f02e112bcd14e89649d8f7c0e92bb1cf6c582f95
ElTinmar <martin.privat@hotmail.fr>
2025-04-11 17:11:32 +0200

    update

--- ds18b20/__init__.py
+++ ds18b20/__init__.py
@@ -3,9 +3,10 @@ import serial
 class CommunicationError(serial.SerialException):
     ... 
 
-def read_temperature_celsius() -> float:
+def read_temperature_celsius(port: str = '/dev/ttyUSB0') -> float:
+    
     try:
-        ser = serial.Serial('/dev/ttyUSB0', baudrate = 9600)
+        ser = serial.Serial(port, baudrate = 9600)
         return float(ser.readline())
     
     except:
