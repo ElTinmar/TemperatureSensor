fb2905d7151a915461b332de99c534a09d457e96
ElTinmar <martin.privat@hotmail.fr>
2025-04-14 11:58:34 +0200

    context manager

--- ds18b20/__init__.py
+++ ds18b20/__init__.py
@@ -6,8 +6,8 @@ class CommunicationError(Exception):
 def read_temperature_celsius(port: str = '/dev/ttyUSB0') -> float:
     
     try:
-        ser = serial.Serial(port, baudrate = 9600)
-        return float(ser.readline())
+        with serial.Serial(port, baudrate = 9600) as ser:
+            return float(ser.readline())
     
     except Exception as e:
         raise CommunicationError('Temperature sensor error') from e
