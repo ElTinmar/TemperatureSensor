3e87b8f9fd0beb93a1711bd3ad7ffa0d5221c449
ElTinmar <martin.privat@hotmail.fr>
2025-04-14 18:16:39 +0200

    added timeout

--- ds18b20/__init__.py
+++ ds18b20/__init__.py
@@ -3,10 +3,10 @@ import serial
 class CommunicationError(Exception):
     ... 
 
-def read_temperature_celsius(port: str = '/dev/ttyUSB0') -> float:
+def read_temperature_celsius(port: str = '/dev/ttyUSB0', timeout: float = 2) -> float:
     
     try:
-        ser = serial.Serial(port, baudrate = 9600)
+        ser = serial.Serial(port, baudrate = 9600,  timeout=timeout)
         return float(ser.readline())
     
     except Exception as e:
